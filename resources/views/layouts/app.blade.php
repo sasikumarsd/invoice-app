42a2c1aae8449b841d69ef1032b191d9abb1dc63
sasikumarsd <sasikr2000@gmail.com>
2025-07-18 13:57:30 +0530

    Alert message button changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title', 'Invoice System')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <nav class="navbar navbar-dark bg-dark mb-4">
